e40471491cf8691e0a43902a32e9cd80c2974b29
jjuser0110 <163217224+jjuser0110@users.noreply.github.com>
2026-02-09 00:03:15 +0800

    aaa

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -31,6 +31,16 @@ $currentRoute = request()->route()->getName();
                 <div>{{ __('sidebar.dashboard') }}</div>
             </a>
         </li>
+        {{-- Settings --}}
+        <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">{{ __('sidebar.reports') }}</span>
+        </li>
+        <li class="menu-item {{ Str::contains($currentRoute, 'report.daily_report') ? 'active' : ''}}">
+            <a href="{{ route('report.daily_report') }}" class="menu-link" onclick="showLoading()">
+                <i class="menu-icon tf-icons bx bx-line-chart"></i>
+                <div>{{ __('sidebar.daily_report') }}</div>
+            </a>
+        </li>
         @endif
 
         {{-- Pendings --}}
@@ -122,16 +132,6 @@ $currentRoute = request()->route()->getName();
             </a>
         </li>
 
-        {{-- Settings --}}
-        <li class="menu-header small text-uppercase">
-            <span class="menu-header-text">{{ __('sidebar.reports') }}</span>
-        </li>
-        <li class="menu-item {{ Str::contains($currentRoute, 'report.daily_report') ? 'active' : ''}}">
-            <a href="{{ route('report.daily_report') }}" class="menu-link" onclick="showLoading()">
-                <i class="menu-icon tf-icons bx bx-line-chart"></i>
-                <div>{{ __('sidebar.daily_report') }}</div>
-            </a>
-        </li>
         @endif
     </ul>
 </aside>
