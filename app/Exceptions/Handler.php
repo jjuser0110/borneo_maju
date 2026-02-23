0da73b1a537101f044b796941bd0bbb896a6239a
jjuser0110 <163217224+jjuser0110@users.noreply.github.com>
2026-02-23 16:55:26 +0800

    aaa

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,8 +24,17 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (TokenMismatchException $e, $request) {
+
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'message' => 'Session expired. Please login again.'
+                ], 419);
+            }
+
+            return redirect()
+                ->route('login') // make sure your login route name is correct
+                ->with('error', 'Your session expired. Please login again.');
         });
     }
 }
